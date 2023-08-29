ee93b42293825ff825a7b0e7d81c981b0bfff091
SevenStreams <sevenstr@student.21-school.ru>
2023-08-29 21:46:33 +0700

    starting vector

---
+++ lib/s21_vector.h
@@ -0,0 +1,12 @@
+#ifndef S21_CONTAINERS_LIB_S21_VECTOR_H_
+#define S21_CONTAINERS_LIB_S21_VECTOR_H_
+
+class Vector {
+ public:
+  
+
+ private:
+  
+};
+
+#endif  // S21_CONTAINERS_LIB_S21_VECTOR_H_
