837b728b83246fb1f8e3dbc2764c93f47f989e1a
SevenStreams <sevenstr@student.21-school.ru>
2023-09-03 19:46:51 +0700

    moved to .h

--- lib/s21_vector.cc
+++ lib/s21_vector.cc
@@ -1,10 +1,1 @@
 #include "s21_vector.h"
-
-#include <iostream.h>
-
-template<class T> // , class Allocator = std::allocator<T>
-class Vector {
-    T* data;
-    size_t size;
-    size_t capacity;
-};
--- lib/s21_vector.h
+++ lib/s21_vector.h
@@ -1,12 +1,16 @@
 #ifndef S21_CONTAINERS_LIB_S21_VECTOR_H_
 #define S21_CONTAINERS_LIB_S21_VECTOR_H_
 
+#include "stddef.h"
+
+template <class T>  // , class Allocator = std::allocator<T>
 class Vector {
- public:
-  
+  // public:
 
  private:
-  
+  T* data;
+  size_t size;
+  size_t capacity;
 };
 
 #endif  // S21_CONTAINERS_LIB_S21_VECTOR_H_
