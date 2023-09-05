ea77f2ef190101fb6dd1196b7a9a3b70433c4925
SevenStreams <sevenstr@student.21-school.ru>
2023-09-05 21:16:14 +0700

    working funcs for vector

---
+++ lib/containers.h
@@ -0,0 +1,6 @@
+#ifndef CONTAINERS_LIB_CONTAINERS_H_
+#define CONTAINERS_LIB_CONTAINERS_H_
+
+#include "vector.h"
+
+#endif  // CONTAINERS_LIB_CONTAINERS_H_
---
+++ lib/vector.h
@@ -0,0 +1,99 @@
+#ifndef CONTAINERS_LIB_VECTOR_H_
+#define CONTAINERS_LIB_VECTOR_H_
+
+#include <cstddef>
+#include <iostream>
+#include <memory>
+
+template <class T>  // , class Allocator = std::allocator<T>
+class Vector {
+  T* data;
+  size_t v_size;
+  size_t v_capacity;
+
+ public:
+  // --------------------- MEMBER FUNCTION ---------------------
+  Vector() noexcept : data(nullptr), v_size(0), v_capacity(0){};
+
+  // T& operator=();
+
+  // --------------------- ELEMENT ACCESS ---------------------
+  T& operator[](size_t i) { return data[i]; };
+  const T& operator[](size_t i) const { return data[i]; };
+  T& at(size_t i) {
+    if (i >= v_size) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data[i];
+  };
+
+  // --------------------- CAPACITY ---------------------
+  size_t size() const noexcept { return v_size; };
+
+  size_t capacity() const noexcept { return v_capacity; };
+
+  void reserve(size_t n) {
+    if (n <= v_capacity) return;
+    T* new_data = reinterpret_cast<T*>(new char[n * sizeof(T)]);
+    try {
+      std::uninitialized_copy(data, data + v_size, new_data);
+    } catch (...) {
+      delete[] reinterpret_cast<char*>(new_data);
+      throw;
+    }
+    for (size_t i = 0; i < v_size; ++i) {
+      data[i].~T();
+    }
+    delete[] reinterpret_cast<char*>(data);
+    data = new_data;
+    v_capacity = n;
+  };
+
+  // void shrink_to_fit();
+
+  // --------------------- ITERATORS ---------------------
+
+  // --------------------- MODIFIERS ---------------------
+  void resize(size_t n, const T& value = T()) {
+    if (n > v_capacity) reserve(n);
+    size_t i = v_size;
+    try {
+      for (; i < n; ++i) {
+        new (data + i) T(value);
+      }
+    } catch (...) {
+      for (size_t j = v_size; j < i; ++j) {
+        data[j].~T();
+      }
+      throw;
+    }
+    if (n < v_size) {
+      v_size = n;
+    }
+  };
+
+  void push_back(const T& value) {
+    if ((v_capacity == v_size)) {
+      reserve(2 * v_size + 1);
+    }
+    try {
+      new (data + v_size) T(value);
+      ++v_size;
+    } catch (...) {
+      data[v_size].~T();
+      throw;
+    }
+  };
+
+  void pop_back() {
+    --v_size;
+    data[v_size].~T();
+  };
+
+  // private:
+};
+
+template <>
+class Vector<bool> {};
+
+#endif  // CONTAINERS_LIB_VECTOR_H_
