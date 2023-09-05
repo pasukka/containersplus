2c78b4cd51dfb1f4ab1b17acfaf2c9caca763875
SevenStreams <sevenstr@student.21-school.ru>
2023-09-05 18:07:25 +0700

    added templ vector bool

--- lib/s21_vector.cc
+++ lib/s21_vector.cc
@@ -1,5 +1,26 @@
 #include "s21_vector.h"
 
+// --------------------- MEMBER FUNCTION ---------------------
+
+
+// --------------------- ELEMENT ACCESS ---------------------
+
+template<class T> T& Vector<T>::operator[](size_t i) {
+    return data[i];
+}
+
+template<class T> const T& Vector<T>::operator[](size_t i) const {
+    return data[i];
+}
+
+template<class T> T& Vector<T>::at(size_t i) {
+    if (i >= v_size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data[i];
+}
+
+// --------------------- CAPACITY ---------------------
 template<class T> size_t Vector<T>::size() const noexcept {
     return v_size;
 }
@@ -25,20 +46,17 @@ template<class T> void Vector<T>::reserve(size_t n) {
     v_capacity = n;
 }
 
+
+// --------------------- ITERATORS ---------------------
+
+
+// --------------------- MODIFIERS ---------------------
+
 template<class T> void Vector<T>::resize(size_t n, const T& value = T()) {
     if (n > v_capacity) reserve(n);
-    size_t i = v_size
-    try {
-        for (; i < n; ++i) {
-            new (data + i) T(value);
-        }
-    } catch (...) {
-        for (size_t j = v_size; j < i, ++j) {
-            data[i].~T();
-        }
-        delete[] reinterpret_cast<char*>(new_data);
-        throw;
-    }    
+    for (size_t i = v_size; i < n; ++i) {
+        new (data + i) T(value);  // TODO: try catch
+    }
     if (n < v_size) {
         v_size = n;
     }
@@ -54,4 +72,6 @@ template<class T> void Vector<T>::push_back(const T& value) {
 
 template<class T> void Vector<T>::pop_back() {
     --v_size;
+    data[v_size].~T();
 }
+
--- lib/s21_vector.h
+++ lib/s21_vector.h
@@ -1,6 +1,8 @@
 #ifndef S21_CONTAINERS_LIB_S21_VECTOR_H_
 #define S21_CONTAINERS_LIB_S21_VECTOR_H_
 
+#include <iostream>
+#include <memory>
 #include <cstddef>
 
 template <typename T>  // , class Allocator = std::allocator<T>
@@ -10,21 +12,28 @@ class Vector {
   size_t v_capacity;
   
  public:
+  // T& operator=();
+
+  T& operator[](size_t i);
+  const T& operator[](size_t i) const;
+  T& at(size_t i);
+
   size_t size() const noexcept;
   size_t capacity() const noexcept;
-
   void reserve(size_t n);
+  // void shrink_to_fit();
+  
   void resize(size_t n, const T& value = T());
-  void shrink_to_fit();
-
   void push_back(const T& value);
   void pop_back();
 
-
-
-
  // private:
   
+};
+
+template <>
+class Vector<bool> {
+
 };
 
 #endif  // S21_CONTAINERS_LIB_S21_VECTOR_H_
