cfe8141c3bc1e0aa7c676efd7729fe6ec7511860
SevenStreams <sevenstr@student.21-school.ru>
2023-10-14 11:54:21 +0700

    map

--- lib/map.h
+++ lib/map.h
@@ -8,7 +8,7 @@ template <class Key, class T, class Compare = std::less<Key>>
 using map = std::map<Key, T, Compare,
                      std::pmr::polymorphic_allocator<std::pair<const Key, T>>>;
 }
-using valuetype =  std::pair<const Key, T>;
+using valuetype = std::pair<const Key, T>;
 using size_type = std::size_t;
 
 #endif  // CONTAINERS_LIB_MAP_H_
