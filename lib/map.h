fb59283730ec4caf7c2067fa79c10cc8d8654a3d
SevenStreams <sevenstr@student.21-school.ru>
2023-10-14 11:29:39 +0700

    started map

---
+++ lib/map.h
@@ -0,0 +1,14 @@
+#ifndef CONTAINERS_LIB_MAP_H_
+#define CONTAINERS_LIB_MAP_H_
+
+#include <iostream>
+
+namespace pmr {
+template <class Key, class T, class Compare = std::less<Key>>
+using map = std::map<Key, T, Compare,
+                     std::pmr::polymorphic_allocator<std::pair<const Key, T>>>;
+}
+using valuetype =  std::pair<const Key, T>;
+using size_type = std::size_t;
+
+#endif  // CONTAINERS_LIB_MAP_H_
