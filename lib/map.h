56402545d3111ea8b34f86ffb2f913db8087fdc9
SevenStreams <sevenstr@student.21-school.ru>
2023-10-20 17:13:50 +0700

    using list

--- lib/map.h
+++ lib/map.h
@@ -1,14 +1,31 @@
 #ifndef CONTAINERS_LIB_MAP_H_
 #define CONTAINERS_LIB_MAP_H_
 
+#include <functional>
 #include <iostream>
 
-namespace pmr {
-template <class Key, class T, class Compare = std::less<Key>>
-using map = std::map<Key, T, Compare,
-                     std::pmr::polymorphic_allocator<std::pair<const Key, T>>>;
-}
-using valuetype = std::pair<const Key, T>;
-using size_type = std::size_t;
+template <class Key, class T, class Compare = std::less<Key>,
+          class Allocator = std::allocator<std::pair<const Key, T>>>
+class map {
+  using key_type = Key;
+  using mapped_type = T;
+  using valuetype = std::pair<const Key, T>;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using key_compare = Compare;
+  using allocator_type = Allocator;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+
+  using pointer = std::allocator_traits<Allocator>::pointer;
+  using const_pointer = std::allocator_traits<Allocator>::const_pointer;
+  using iterator = pointer;
+  using const_iterator = const_pointer;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator= std::reverse_iterator<const_iterator>;
+
+
+  map(): map(Compare()) {}
+};
 
 #endif  // CONTAINERS_LIB_MAP_H_
