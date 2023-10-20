24b31149bf335d2617eca0318c9a880f7b7b1742
SevenStreams <sevenstr@student.21-school.ru>
2023-10-20 17:54:24 +0700

    adaptation of some funcs from vector

--- lib/map.h
+++ lib/map.h
@@ -22,10 +22,103 @@ class map {
   using iterator = pointer;
   using const_iterator = const_pointer;
   using reverse_iterator = std::reverse_iterator<iterator>;
-  using const_reverse_iterator= std::reverse_iterator<const_iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+ public:
+  // --------------------- MEMBER FUNCTION ---------------------
 
-  map(): map(Compare()) {}
+  // ------- Constructors -------
+  map() : data_(nullptr), size_(0), capacity_(0), alloc_(Allocator()) {}
+  
+  map() : map(Compare()) {}
+
+  explicit map( const Compare& comp, const Allocator& alloc = Allocator() ) {};
+
+  // ------- Destructors -------
+
+  // ------- Copy -------
+
+  map& operator=(const map& other) {};
+
+  map& operator=(map&& other) noexcept {};
+
+  map& operator=(std::initializer_list<value_type> ilist) {};
+
+  allocator_type get_allocator() const noexcept { return alloc_; };
+
+  // ------- Element access -------
+
+  T& at(const Key& key){};
+
+  const T& at(const Key& key) const {
+
+  };
+
+  T& operator[](const Key& key){};
+
+  T& operator[](Key&& key){};
+
+  // ------- Iterators -------
+
+  iterator begin() { return &data_[0]; }
+  const_iterator begin() const noexcept { return &data_[0]; };
+  const_iterator cbegin() const noexcept { return &data_[0]; };
+
+  iterator end() { return &data_[size_]; }
+  const_iterator end() const noexcept {return &data_[size_]};
+  const_iterator cend() const noexcept {return &data_[size_]};
+
+  reverse_iterator rbegin() noexcept { return reverse_iterator(end()); };
+  reverse_iterator rend() noexcept { return reverse_iterator(begin()); };
+
+  const_reverse_iterator rbegin() const noexcept {
+    return const_reverse_iterator(end());
+  };
+
+  const_reverse_iterator crbegin() const noexcept {
+    return const_reverse_iterator(cend());
+  };
+
+  const_reverse_iterator rend() const noexcept {
+    return const_reverse_iterator(begin());
+  };
+
+  const_reverse_iterator crend() const noexcept {
+    return const_reverse_iterator(cbegin());
+  };
+
+  // ------- Capacity -------
+
+  bool empty() { return (size_ == 0); }
+
+  size_type size() const noexcept { return size_; };
+
+  size_type max_size() const noexcept {
+    return alloc_traits::max_size(alloc_);
+  };
+
+  // ------- Modifiers -------
+
+  void clear() noexcept { size_ = 0; };
+
+  void swap(map& other) noexcept {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(alloc_, other.alloc_);
+  };
+
+  // ------- Lookup -------
+
+  // ------- Observers -------
+  //  key_comp
+  // value_comp
+
+ private:
+  pointer data_;
+  size_type size_;
+  size_type capacity_;
+  Allocator alloc_;
 };
 
 #endif  // CONTAINERS_LIB_MAP_H_
