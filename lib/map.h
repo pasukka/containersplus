cbe3c0bbe5a4edf7c973727f7b1c1f67bcfddf0b
SevenStreams <sevenstr@student.21-school.ru>
2023-10-24 23:05:31 +0700

    insert, [], iterators

--- lib/map.h
+++ lib/map.h
@@ -27,53 +27,37 @@ class map {
   using const_reverse_iterator = std::reverse_iterator<const_iterator>;
   using alloc_traits = std::allocator_traits<Allocator>;
 
+  using BTree = tree<Key, T, Compare, Allocator>;
+
  public:
-  class Node {
-   public:
-    // Node() : data(nullptr), parent(nullptr), left(nullptr), right(nullptr){};
-    Node(value_type data)
-        : data(data), parent(nullptr), left(nullptr), right(nullptr){};
-
-  //  private:
-    value_type data;
-    Node *parent;
-    Node *left;
-    Node *right;
-  };
-
   // --------------------- MEMBER FUNCTION ---------------------
 
   // ------- Constructors -------
-  map() : data_(nullptr), size_(0), alloc_(Allocator()){};
+  map() : root(){};
 
-  // map() : map(Compare()) {};
+  // explicit map(const Compare &comp, const Allocator &alloc = Allocator()){};
 
-  explicit map(const Compare &comp, const Allocator &alloc = Allocator()){};
+  map(const map &other) : root(other.root) {};
 
-  map(const map &other) {
-    alloc_ = other.alloc_;
-    copy_elements(other);
-  };
+  // map(const map &other, const Allocator &alloc) {
+  //   alloc_ = alloc;
+  //   copy_elements(other);
+  // };
 
-  map(const map &other, const Allocator &alloc) {
-    alloc_ = alloc;
-    copy_elements(other);
-  };
+  // map(map &&other) noexcept
+  //     : data_(other.data_), size_(other.size_), alloc_(other.alloc_) {
+  //   other.data_ = nullptr;
+  //   other.size_ = 0;
+  //   other.alloc_ = Allocator();
+  // };
 
-  map(map &&other) noexcept
-      : data_(other.data_), size_(other.size_), alloc_(other.alloc_) {
-    other.data_ = nullptr;
-    other.size_ = 0;
-    other.alloc_ = Allocator();
-  };
+  // map(map &&other, const Allocator &alloc) noexcept : map(other) {
+  //   alloc_ = alloc;
+  //   other.alloc_ = Allocator();
+  // };
 
-  map(map &&other, const Allocator &alloc) noexcept : map(other) {
-    alloc_ = alloc;
-    other.alloc_ = Allocator();
-  };
-
-  map(std::initializer_list<value_type> init, const Allocator &alloc)
-      : map(init, Compare(), alloc) {}
+  // map(std::initializer_list<value_type> init, const Allocator &alloc)
+  //     : map(init, Compare(), alloc) {}
 
   // ------- Destructors -------
   ~map(){};
@@ -85,7 +69,7 @@ class map {
 
   map &operator=(std::initializer_list<value_type> ilist){};
 
-  allocator_type get_allocator() const noexcept { return alloc_; };
+  allocator_type get_allocator() const noexcept { return root.alloc_; };
 
   // ------- Element access -------
   T &at(const Key &key){};
@@ -93,24 +77,23 @@ class map {
   const T &at(const Key &key) const {};
 
   T &operator[](const Key &key) {
-    // return iter on find
-    return data_[0].data.second;
+    iterator it = iterator(root.find_node(root.data_, key));
+    return (*it).second;
   };
 
   T &operator[](Key &&key) {
-    // return iter on find
-    
-    return data_[0].data.second;
+    iterator it = iterator(root.find_node(root.data_, key));
+    return (*it).second;
   };
 
   // ------- Iterators -------
-  iterator begin() { return iterator(&data_[0]); };
-  const_iterator begin() const noexcept { return &data_[0]; };
-  const_iterator cbegin() const noexcept { return &data_[0]; };
+  iterator begin() { return iterator(root.data_); };
+  const_iterator begin() const noexcept { return root.data_[0]; };
+  const_iterator cbegin() const noexcept { return root.data_[0]; };
 
-  iterator end() { return &data_[size_]; };
-  const_iterator end() const noexcept { return &data_[size_]; };
-  const_iterator cend() const noexcept { return &data_[size_]; };
+  iterator end() { return iterator(root.data_ + root.size_); };
+  const_iterator end() const noexcept { return root.data_[root.size_]; };
+  const_iterator cend() const noexcept { return root.data_[root.size_]; };
 
   reverse_iterator rbegin() noexcept { return reverse_iterator(end()); };
   reverse_iterator rend() noexcept { return reverse_iterator(begin()); };
@@ -132,20 +115,20 @@ class map {
   };
 
   // ------- Capacity -------
-  bool empty() const { return (size_ == 0); }
+  bool empty() const { return (root.size_ == 0); }
 
-  size_type size() const noexcept { return size_; };
+  size_type size() const noexcept { return root.size_; };
 
   size_type max_size() const noexcept {
-    return alloc_traits::max_size(alloc_);
+    return alloc_traits::max_size(root.alloc_);
   };
 
   // ------- Modifiers -------
-  void clear() noexcept { size_ = 0; };
+  void clear() noexcept { root.size_ = 0; };
 
   // insert
   std::pair<iterator, bool> insert(const value_type &value) {
-    return insert_unique(value);
+    return root.insert_unique(value);
   };
 
   // insert_or_assign
@@ -161,9 +144,9 @@ class map {
   // erase
 
   void swap(map &other) noexcept {
-    std::swap(data_, other.data_);
-    std::swap(size_, other.size_);
-    std::swap(alloc_, other.alloc_);
+    std::swap(root.data_, other.data_);
+    std::swap(root.size_, other.size_);
+    std::swap(root.alloc_, other.alloc_);
   };
 
   // extract
@@ -177,59 +160,26 @@ class map {
   // value_comp
 
  private:
-  Node* data_;
-  size_type size_;
-  Allocator alloc_;
+  BTree root;
 
-  void copy_elements(map &&other) {
-    int count = 0;
-    try {
-      for (iterator i = &other.data_[0]; i != &other.data_[other.size_];
-           ++i, ++count) {
-        alloc_.construct(&data_[count], *i);
-      }
-    } catch (...) {
-      destroy_elements(0, count);
-      throw;
-    }
-  };
+  // void copy_elements(map &&other) {
+  //   int count = 0;
+  //   try {
+  //     for (iterator i = &other.data_[0]; i != &other.data_[other.size_];
+  //          ++i, ++count) {
+  //       root.alloc_.construct(&data_[count], *i);
+  //     }
+  //   } catch (...) {
+  //     destroy_elements(0, count);
+  //     throw;
+  //   }
+  // };
 
-  void destroy_elements(size_type start, size_type count) {
-    for (size_type i = start; i < count; ++i) {
-      alloc_.destroy(data_ + i);
-    }
-  }
-
-  std::pair<iterator, bool> insert_unique(const value_type& value) {
-    bool insert = false;
-    if (data_ == nullptr) {
-      
-      Node* newNode = new Node(value);
-      newNode->right = nullptr;
-      newNode->left = nullptr;
-      newNode->parent = nullptr;
-      data_ = newNode;
-      ++size_;
-      insert = true;
-      
-    }
-    if (!insert) {
-      return insert_unique_pos(value.first, value.second);
-    }
-    return std::pair<iterator, bool>(iterator(begin()), insert);
-  };
-
-  std::pair<iterator, bool> insert_unique_pos(Key key, T value) {
-    bool insert = false;
-    Node* newNode = new Node(value_type(key, value));
-    newNode->right = nullptr;
-    newNode->left = nullptr;
-    newNode->parent = nullptr;
-    ++size_;
-    return std::pair<iterator, bool>(iterator(newNode), insert);
-  };
-
-
+  // void destroy_elements(size_type start, size_type count) {
+  //   for (size_type i = start; i < count; ++i) {
+  //     root.alloc_.destroy(data_ + i);
+  //   }
+  // }
 };
 
 #endif  // CONTAINERS_LIB_MAP_H_
