30b5b65bf7bf67c12d4f22d1b5cf94a464f99548
SevenStreams <sevenstr@student.21-school.ru>
2023-10-29 21:27:54 +0700

    test for insert with []

--- test/test_map.cc
+++ test/test_map.cc
@@ -47,50 +47,52 @@ TEST(MapTestInsert, SimpleInsertInt2) {
   EXPECT_EQ(val, map1[key]);
 }
 
-TEST(MapTestIter, IteratorBeginEnd) {
-  map<int, float> num_map;
-  std::vector v1 = {4, 9, 1};
-  std::vector v2 = {4.13, 9.24, 1.09};
-
-  num_map.insert({4, 4.13});
-  num_map.insert({9, 9.24});
-  num_map.insert({1, 1.09});
-  EXPECT_EQ(3, num_map.size());
-
-  size_t i = 0;
-  for (auto it = num_map.begin(); it != num_map.end(); ++it, ++i) {
-    // printf("\n %d %f \n", , );
-    EXPECT_EQ(it->first, v1[i]);
-    EXPECT_EQ(it->second, v2[i]);
-  }
-}
-
-TEST(MapTestAccess, AccessNoKey_1) {
+TEST(MapTestInsert, InsertInt) {
   map<int, int> map1;
   int val = 60;
   int key = 2;
-  map1.insert({key, val});
-  try {
-    map1[1];
-    FAIL();
-  } catch (std::out_of_range &ex) {
-    SUCCEED();
-  }
+  map1[key] = val;
+  EXPECT_EQ(val, map1[key]);
 }
 
-TEST(MapTestAccess, AccessNoKey_2) {
-  map<int, int> map1;
-  int val = 60;
-  int key = 2;
-  map1.insert({key, val});
-  --key;
-  try {
-    map1[key];
-    FAIL();
-  } catch (std::out_of_range &ex) {
-    SUCCEED();
-  }
-}
+// TEST(MapTestIter, IteratorBeginEnd) {
+//   map<int, float> num_map;
+//   std::vector v1 = {4, 9, 1};
+//   std::vector v2 = {4.13, 9.24, 1.09};
+
+//   num_map.insert({4, 4.13});
+//   num_map.insert({9, 9.24});
+//   num_map.insert({1, 1.09});
+//   EXPECT_EQ(3, num_map.size());
+
+//   size_t i = 0;
+//   for (auto it = num_map.begin(); it != num_map.end(); ++it, ++i) {
+//     EXPECT_EQ(it->first, v1[i]);
+//     EXPECT_EQ(it->second, v2[i]);
+//   }
+// }
+
+// TEST(MapTestAccess, AccessNoKey_1) {
+//   map<int, int> map1;
+//   int val = 60;
+//   int key = 2;
+//   map1.insert({key, val});
+//   EXPECT_EQ(0, map1[2]);
+// }
+
+// TEST(MapTestAccess, AccessNoKey_2) {
+//   map<int, int> map1;
+//   int val = 60;
+//   int key = 2;
+//   map1.insert({key, val});
+//   --key;
+//   try {
+//     map1[key];
+//     FAIL();
+//   } catch (std::out_of_range &ex) {
+//     SUCCEED();
+//   }
+// }
 
 TEST(MapTestAt, AtKey_1) {
   map<int, int> map1;
@@ -105,19 +107,30 @@ TEST(MapTestAt, AtKey_2) {
   int val = 60;
   int key = 2;
   map1.insert({key, val});
-  map1.insert({key+1, val+1});
-  EXPECT_EQ(val+1, map1.at(key+1));
+  map1.insert({key + 1, val + 1});
+  EXPECT_EQ(val + 1, map1.at(key + 1));
 }
 
-TEST(MapTestAt, AtNoKey) {
+TEST(MapTestAt, AtKey_3) {
   map<int, int> map1;
   int val = 60;
   int key = 2;
   map1.insert({key, val});
-  try {
-    map1.at(1);
-    FAIL();
-  } catch (...) {
-    SUCCEED();
-  }
+  map1.insert({key + 1, val + 1});
+  map1.insert({key + 2, val + 2});
+  map1.insert({key + 23, val + 23});
+  EXPECT_EQ(val + 23, map1.at(key + 23));
 }
+
+// TEST(MapTestAt, AtNoKey) {
+//   map<int, int> map1;
+//   int val = 60;
+//   int key = 2;
+//   map1.insert({key, val});
+//   try {
+//     map1.at(1);
+//     FAIL();
+//   } catch (...) {
+//     SUCCEED();
+//   }
+// }
